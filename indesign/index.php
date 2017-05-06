22d642a54c1b0df4433a168f956fee794e2738d7
lcdswat <lcd-swat@yandex.ru>
2017-05-06 12:52:17 +0300

    upload

---
+++ indesign/index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="indesign.css">
+	<script type="text/javascript" src="https://code.jquery.com/jquery-latest.min.js"></script>
+	<script type="text/javascript" src="jquery.stellar.min.js"></script>
+</head>
+<body>
+
+<div class="bg" data-stellar-ratio="1.2"></div>
+	
+<!-- Контент -->
+<div id="grid-bg">
+	<div class="grid">
+		<!-- Заголовок -->
+		<h1>Верстаем<br>быстро</h1>
+		
+		<!-- Осталось мест -->
+		<div id="place-count">
+			<span>10/10</span>
+			Осталось десять свободных&nbsp;мест.<br><a href="/">Записаться</a>.
+		</div>
+		<div class="clean"></div>
+		<p>Двухдневный интенсив Марата Бакирова<br>на&nbsp;тему оптимизации вёрстки в&nbsp;индизайне.</p>
+
+		<!-- Что где когда -->
+		<div class="column">
+			10—11 апреля<br>С 12:00 до 16:00
+		</div>
+		<div class="column">
+			«Фактуры»<br><a href="/">Уфа, ул. Мендеелева, 205а</a>
+		</div>
+		<div class="column" style="margin: 125px 0 0 0;">
+			Стоимость участия<br>2000 рублей
+		</div>
+		<div class="clean"></div>
+
+		<!-- Панель стиль абзаца -->
+		<div id="panel-one">
+			<span id="style-one"></span>
+			<span id="style-two"></span>
+			<span id="style-three"></span>
+			<span id="style-four"></span>
+			<span id="style-five"></span>
+			<span id="style-six"></span>
+		</div>
+
+		<div class="clean"></div>
+
+		<!-- Кому будет полезно -->
+		<h2 id="for-who-podzag">Кому это будет полезно?</h2>
+		<p id="for-who">Интенсив расчитан на&nbsp;дизайнеров и&nbsp;верстальщиков, которые хотя&nbsp;бы приблизительно знакомы с&nbsp;индизайном и&nbsp;хотят повысить навыки работы с&nbsp;ним. В&nbsp;программе не&nbsp;будет размышлений о&nbsp;модульных сетках, типографике и&nbsp;стиле. Только прикладные советы направленные на&nbsp;облегчение и&nbsp;ускорение вёрстки в&nbsp;индизайне.</p>
+
+		<div class="programm-column">
+		<h2>1 день &mdash; теория</h2>
+		<p>В&nbsp;первый день я&nbsp;расскажу всё что знаю об&nbsp;индизайне и&nbsp;о&nbsp;том как использовать эту программу эффективно для любой вёрстки&nbsp;&mdash; от&nbsp;книг до&nbsp;веба.<br><br>
+		В&nbsp;программе: подготовка рабочего окружения; горячие клавиши; стили абзаца и&nbsp;символа; использование GREP; вёрстка таблиц, объединение данных и&nbsp;прочие маленькие хитрости.</p>
+		</div>
+
+		<div class="programm-column" style="margin: 220px 0 0 0;">
+		<h2>2 день &mdash; практика</h2>
+		<p>Во&nbsp;второй день вы&nbsp;сможете применить полученные знания на&nbsp;практике&nbsp;&mdash; каждому участнику будет выдан материал для вёрстки, который нужно будет закончить в&nbsp;определенный срок.<br><br>
+		В&nbsp;процессе работы я&nbsp;буду отвечать на&nbsp;возникающие вопросы и&nbsp;подсказывать</p>
+		</div>
+		
+		<div class="clean"></div>
+
+		<!-- Панель скриптов -->
+		<div id="panel-two">
+			<span id="typograf"></span>
+		</div>
+
+		<div class="about-me" id="szamedless">
+			<h2>Немного обо мне -</h2>
+			<p>Меня зовут Марат Бакиров. Так уж случилось, что последние 10 лет каждый рабочий день первая программа, которую я запускаю на компьютере - это индизайн.
+			<br><br>
+			Впервые с многостраничной вёрсткой я столкнулся в 2007 году, когда случайно занял место уволившегося верстальщика в местном журнале. Свой первый номер я верстал 4 суток почти без перерывов на сон и еду и уже тогда понял - тут что-то не так.
+			<br><br>
+			С тех пор прошло много тысяч проведенных за вёрсткой часов и теперь я хочу поделиться некоторыми советами и приемами которые знаю с начинающими дизайнерами.</p>
+		</div>
+
+		<div class="about-me" id="szamed" style="display: none;">
+			<h2>Немного обо мне &mdash;</h2>
+			<p>Меня зовут Марат Бакиров. Так уж случилось, что&nbsp;последние 10&nbsp;лет каждый рабочий день первая программа, которую я запускаю на компьютере&nbsp;&mdash; это&nbsp;индизайн.
+			<br><br>
+			Впервые с многостраничной вёрсткой я столкнулся в&nbsp;2007 году, когда случайно занял место уволившегося верстальщика в местном журнале. Свой первый номер я верстал 4 суток почти без перерывов на&nbsp;сон и&nbsp;еду&nbsp;и уже тогда понял&nbsp;&mdash; тут что-то не так.
+			<br><br>
+			С тех пор прошло много тысяч проведенных за&nbsp;вёрсткой часов&nbsp;и теперь я хочу поделиться некоторыми советами&nbsp;и приемами которые знаю с начинающими дизайнерами.</p>
+		</div>
+
+		<div id="photo"></div>
+
+		<div id="photo-text">Волосяные шпации преследуют меня во&nbsp;сне</div>
+
+		<div class="clean"></div>
+
+		<h2>Хотите принять участие?</h2>
+		<p>
+			Чтобы записаться на&nbsp;интенсив<br>
+			свяжитесь со&nbsp;мной любым способом:<br>
+			&mdash;&nbsp;вконтакте <span class="data-merge"><a href="https://vk.com/lcdswat">vk.com/lcdswat</a></span><span class="data-no-merge"><<Линк vk>></span>;<br>
+			&mdash;&nbsp;по&nbsp;электронной почте <span class="data-merge"><a href="mailto:lcd-swat@yandex.ru">lcd-swat@yandex.ru</a></span><span class="data-no-merge"><<Почта>></span>.<br>
+			&mdash;&nbsp;по&nbsp;телефону <span class="data-merge">+7&nbsp;999&nbsp;133 15 96</span><span class="data-no-merge"><<Телефон>></span>;
+		</p>
+
+		<!-- Панель данных -->
+		<div id="panel-three">
+			<span id="data-merge-checked"></span>
+		</div>
+
+		<div class="clean"></div>
+
+		<br><br><br><br>
+
+	</div>
+</div>
+
+<!-- Объединение данных -->
+<script type="text/javascript">
+$("#data-merge-checked").click(function(){
+	$("#panel-three").toggleClass("panel-three-checked");
+	$(".data-no-merge").toggleClass("data-merge-yes");
+	$(".data-merge").toggleClass("data-merge-no");
+});
+</script>
+
+<!-- Паралакс -->
+<script type="text/javascript">
+$.stellar();
+</script>
+
+<!-- Убрать гайды -->
+<script type="text/javascript">
+	guides = true;
+	$(document).keyup(function(e) {
+    	if(e.keyCode == 87) {guides = !guides};
+	    if (!guides) {
+	    	$("#grid-bg").css("background-image","none");
+	    }
+	    else {
+	    	$("#grid-bg").css("background-image","url(grid.png)");
+	    }
+	});
+</script>
+
+<!-- Смена класса -->
+<script type="text/javascript">
+$("#style-one").click(function(){
+	$("#for-who-podzag").removeClass();
+	$("#for-who-podzag").addClass("style-one");
+	$("#panel-one").css('background-position-x' , '0px');
+});
+$("#style-two").click(function(){
+	$("#for-who-podzag").removeClass();
+	$("#for-who-podzag").addClass("style-two");
+	$("#panel-one").css('background-position-x' , '-243px');
+});
+$("#style-three").click(function(){
+	$("#for-who-podzag").removeClass();
+	$("#panel-one").css('background-position-x' , '-486px');
+});
+$("#style-four").click(function(){
+	$("#for-who-podzag").removeClass();
+	$("#for-who-podzag").addClass("style-four");
+	$("#panel-one").css('background-position-x' , '-729px');
+});
+$("#style-five").click(function(){
+	$("#for-who-podzag").removeClass();
+	$("#for-who-podzag").addClass("style-five");
+	$("#panel-one").css('background-position-x' , '-972px');
+});
+$("#style-six").click(function(){
+	$("#for-who-podzag").removeClass();
+	$("#for-who-podzag").addClass("style-six");
+	$("#panel-one").css('background-position-x' , '-1215px');
+});
+</script>
+
+<!-- Типограф -->
+<script type="text/javascript">
+$("#typograf").click(function(){
+	$("#szamedless").css('display' , 'none');
+	$("#szamed").css('display' , 'block');
+});
+</script>
+
+<!-- Костыль для винды -->
+<script type="text/javascript">
+var os = 0;
+if (navigator.userAgent.indexOf ('Windows') != -1) os = 1;
+if (os == 1) {
+	$("h1").css('bottom' , '7px');
+	$("h2").css('bottom' , '4px');
+	$("#place-count").css('bottom' , '4px');
+	$(".about-me h2").css('bottom' , '3px');
+	$(".column").css('bottom' , '2px');
+	$(".style-one").css('bottom' , '0px', '!important');
+	$(".style-two").css('bottom' , '7px !important');
+	$(".style-four").css('bottom' , '2px');
+	$(".style-five").css('bottom' , '0px');
+	$(".style-six").css('bottom' , '0px');
+}
+</script>
+
+</body>
+</html>
+
