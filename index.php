a790b2e04fd729b3cedf25c982e1c34d7017de66
lcdswat <lcd-swat@yandex.ru>
2017-05-07 18:17:29 +0300

    upload

--- index.php
+++ index.php
@@ -16,32 +16,42 @@
 	<!-- Меню -->
 	<?php include($_SERVER['DOCUMENT_ROOT'].'/site-files/includes/menu.html'); ?>
 
+	<!-- Галлерея логотипов -->
 	<div class="grid-wide" id="all-logos">
-		<a href="/i/l/lettering-01.png"><img src="/i/l/t/lettering-01.png"></a>
-		<a href="/i/l/lettering-02.png"><img src="/i/l/t/lettering-02.png"></a>
-		<a href="/i/l/lettering-03.png"><img src="/i/l/t/lettering-03.png"></a>
-		<a href="/i/l/lettering-04.png"><img src="/i/l/t/lettering-04.png"></a>
-		<a href="/i/l/lettering-05.png"><img src="/i/l/t/lettering-05.png"></a>
-		<a href="/i/l/lettering-06.png"><img src="/i/l/t/lettering-06.png"></a>
-		<a href="/i/l/lettering-07.png"><img src="/i/l/t/lettering-07.png"></a>
-		<a href="/i/l/lettering-08.png"><img src="/i/l/t/lettering-08.png"></a>
-		<a href="/i/l/lettering-09.png"><img src="/i/l/t/lettering-09.png"></a>
-		<a href="/i/l/lettering-10.png"><img src="/i/l/t/lettering-10.png"></a>
-		<a href="/i/l/lettering-11.png"><img src="/i/l/t/lettering-11.png"></a>
-		<a href="/i/l/lettering-12.png"><img src="/i/l/t/lettering-12.png"></a>
-		<a href="/i/l/lettering-13.png"><img src="/i/l/t/lettering-13.png"></a>
-		<a href="/i/l/lettering-14.png"><img src="/i/l/t/lettering-14.png"></a>
-		<a href="/i/l/lettering-15.png"><img src="/i/l/t/lettering-15.png"></a>
-		<a href="/i/l/lettering-16.png"><img src="/i/l/t/lettering-16.png"></a>
-		<a href="/i/l/lettering-17.png"><img src="/i/l/t/lettering-17.png"></a>
-		<a href="/i/l/lettering-18.png"><img src="/i/l/t/lettering-18.png"></a>
-		<a href="/i/l/lettering-19.png"><img src="/i/l/t/lettering-19.png"></a>
-		<a href="/i/l/lettering-20.png"><img src="/i/l/t/lettering-20.png"></a>
-		<a href="/i/l/lettering-21.png"><img src="/i/l/t/lettering-21.png"></a>
-		<a href="/i/l/lettering-22.png"><img src="/i/l/t/lettering-22.png"></a>
-		<a href="/i/l/lettering-23.png"><img src="/i/l/t/lettering-23.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-01.png"><img src="/i/l/t/lettering-01.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-02.png"><img src="/i/l/t/lettering-02.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-03.png"><img src="/i/l/t/lettering-03.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-04.png"><img src="/i/l/t/lettering-04.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-05.png"><img src="/i/l/t/lettering-05.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-06.png"><img src="/i/l/t/lettering-06.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-07.png"><img src="/i/l/t/lettering-07.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-08.png"><img src="/i/l/t/lettering-08.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-09.png"><img src="/i/l/t/lettering-09.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-10.png"><img src="/i/l/t/lettering-10.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-11.png"><img src="/i/l/t/lettering-11.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-12.png"><img src="/i/l/t/lettering-12.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-13.png"><img src="/i/l/t/lettering-13.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-14.png"><img src="/i/l/t/lettering-14.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-15.png"><img src="/i/l/t/lettering-15.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-16.png"><img src="/i/l/t/lettering-16.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-17.png"><img src="/i/l/t/lettering-17.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-18.png"><img src="/i/l/t/lettering-18.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-19.png"><img src="/i/l/t/lettering-19.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-20.png"><img src="/i/l/t/lettering-20.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-21.png"><img src="/i/l/t/lettering-21.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-22.png"><img src="/i/l/t/lettering-22.png"></a>
+		<a data-fancybox="group" href="/i/l/lettering-23.png"><img src="/i/l/t/lettering-23.png"></a>
 	</div>
 
+
+	<div id="logos" class="center">
+		<img src="/i/l/lettering-01.png">
+		Подпись
+		<img src="/i/l/lettering-02.png">
+		Подпись
+		<img src="/i/l/lettering-03.png">
+		Подпись
+	</div>
 
 	<!-- Настройки фэнсибокса -->
 	<script type="text/javascript">
@@ -50,9 +60,10 @@
 			infobar : true,
 			keyboard : true,
 			fullScreen : false,
-
+			thumbs : false,
+			slideShow : false
 		});
-</script>
+	</script>
 
 </body>
 </html>
